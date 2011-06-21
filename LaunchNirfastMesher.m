4f4da6f030671ec6a2c9353870f3003a5c924421
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2011-06-21 04:08:51 +0000

    Added some documentation to LaunchNirfastMesher.m
    git-svn-id: https://nirfast-meshing.googlecode.com/svn/image2mesh_cgal/trunk@325 fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e

--- LaunchNirfastMesher.m
+++ LaunchNirfastMesher.m
@@ -1,6 +1,14 @@
 function LaunchNirfastMesher(inrfn)
+% This is called from Nirview application to create mesh using segemented
+% images that are saved in Nirview (in .mha format)
 
+% change directory so we are not in Nirview's bin folder
+cd(getuserdir);
+
+% Strip the {} from input .mha file name
 inrfn=inrfn(2:end-1);
+
+% Launch the GUI for mesher
 h=image2mesh_gui;
 data=guidata(h);
 data.inputfn=inrfn;
@@ -8,3 +16,5 @@ guidata(h,data);
 image2mesh_gui('UpdateInputFileInfo',h,[],data);
 data=guidata(h);
 image2mesh_gui('callimage2mesh_cgal_Callback',h,[],data);
+
+fprintf(' Done creating mesh.\n');
