b68bee2b526b1648840b274c357d7a9af654b3a3
hamiid <hamiid@fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e>
2012-08-31 11:41:16 +0000

    Improve logic of handling multi region meshes after optimization
    - This routine now assign new 'element' material to the new mesh
      returnued by stellar optimization algorithm.
    
    git-svn-id: https://nirfast-meshing.googlecode.com/svn/image2mesh_cgal/trunk@417 fb8e7f84-bfd6-7a22-e4a7-981ca257bf3e

--- image2mesh_gui.m
+++ image2mesh_gui.m
@@ -487,12 +487,13 @@ hf = waitbar(0,'Creating mesh, this may take several minutes.');
 % Ask if user wants to optimize quality
 [junk optimize_flag] = optimize_mesh_gui;
 if optimize_flag
-    [genmesh.ele genmesh.node genmesh.region] = ...
+    [genmesh.ele genmesh.node mat] = ...
         call_improve_mesh_use_stellar(e, p);
 else
     genmesh.ele = e;
     genmesh.node = p;
 end
+genmesh.ele(:,5) = mat;
 genmesh.nnpe = 4;
 genmesh.dim = 3;
 
@@ -530,7 +531,8 @@ if ~isempty(handles.sdcoords)
     plot3(handles.sdcoords(:,1),handles.sdcoords(:,2),handles.sdcoords(:,3),'bx');
 end
 
-function [ele node newr] = call_improve_mesh_use_stellar(e, p)
+function [ele node new_elem_region new_node_region] = ...
+    call_improve_mesh_use_stellar(e, p)
 % Check if mesh has region information
 if size(e,2) > 4
     region_ids = unique(e(:,5));
@@ -551,30 +553,46 @@ for i=1:length(region_ids)
 end
 % Improve the mesh
 config.qualmeasure = 0;
-[ie ip] = improve_mesh_use_stellar(e(:,1:4), p, config);
+[ele node] = improve_mesh_use_stellar(e(:,1:4), p, config);
 
 % Since we get a totally new mesh, we need to
 % reassign region propertyies for nodes.
 % Find the closest nodes (from old set 'p') to new set of nodes
 % Meshes can be huge so we only need to examine nodes within a bounding box
 % of the current node being examined.
-newr = zeros(size(ip,1),1,'int8');
+new_node_region = zeros(size(node,1),1,'int8');
 edge_avg = GetEdgeSize(e, p, 4);
 % mesh_bbx = max(p(:,1:3)) - min(p(:,1:3));
 % diag_len = norm(mesh_bbx);
 delta = edge_avg * 2.0;
-for i=1:size(ip,1)
-    bf = abs(ip(i,1) - p(:,1)) < delta & ...
-        abs(ip(i,2) - p(:,2)) < delta & ...
-        abs(ip(i,3) - p(:,3)) < delta;
-    dist = dist2(ip(i,:), p(bf,:));
+for i=1:size(node,1)
+    bf = abs(node(i,1) - p(:,1)) < delta & ...
+        abs(node(i,2) - p(:,2)) < delta & ...
+        abs(node(i,3) - p(:,3)) < delta;
+    dist = dist2(node(i,:), p(bf,:));
     origr = oldr(bf,:);
     [foo idx] = sort(dist, 'ascend');
-    newr(i) = origr(idx(1));
+    new_node_region(i) = origr(idx(1));
 end
 
-ele = ie;
-node = ip;
+% Sort region IDs based on number of nodes they are assigned to
+sumr = zeros(1,length(region_ids));
+new_elem_region = zeros(size(ele,1),1,'int8');
+for i=1:length(region_ids)
+    sumr(i) = sum(new_node_region==region_ids(i));
+end
+[foo idx] = sort(sumr,'ascend');
+region_ids = region_ids(idx);
+% Assign each element a region ID based on how many nodes of each element
+% have been associated with that region ID
+for i=1:4 % nnpe
+    for j=1:length(region_ids)
+        tf1 = ismember(new_node_region(ele(:,1:4)),region_ids(j));
+        nc = sum(tf1,2);
+        new_elem_region(nc == i) = region_ids(j);
+    end
+end
+
 
 function outputfn_Callback(hObject, eventdata, handles)
 % hObject    handle to outputfn (see GCBO)
