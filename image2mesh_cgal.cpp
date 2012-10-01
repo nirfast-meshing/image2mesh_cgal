7105ddd09cab3277dfa409e99b00db87b9cbed86
Hamid Ghadyani <hamid@hamid.cc>
2012-10-01 06:29:53 -0400

    Successful compilation (kinda!)
    - ConstructSeedPoints still needs fixing.

--- image2mesh_cgal.cpp
+++ image2mesh_cgal.cpp
@@ -9,10 +9,13 @@
 #include <CGAL/make_mesh_3.h>
 #include <CGAL/Image_3.h>
 
+#include <vector>
+#include <map>
+
 // Domain
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Labeled_image_mesh_domain_3<CGAL::Image_3,K> Mesh_domain;
-
+typedef Mesh_domain::Index Index;
 // Triangulation
 typedef CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
 typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
@@ -21,12 +24,68 @@ typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
 typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
 typedef CGAL::Mesh_constant_domain_field_3<Mesh_domain::R,
                                            Mesh_domain::Index> Sizing_field;
+typedef Mesh_criteria::Facet_criteria FacetCriteria;
+typedef Mesh_criteria::Cell_criteria CellCriteria;
+
+typedef Mesh_domain::Point_3 PointType;
+typedef Mesh_domain::Point_3 Point_3;
 
 // To avoid verbose function and named parameters call
 using namespace CGAL::parameters;
 
 // Usage: image2mesh_cgal inputstack.inr criteria.txt
 // criterial.txt is a text file containing setting for mesh sizes and refirement options.
+
+template<typename Mesh_domain, typename OutputIterator>
+void ConstructSeedPoints(const CGAL::Image_3 image, const Mesh_domain* domain, const std::map<Index, double>& lengths, OutputIterator pts) {
+	typedef typename MeshDomain::Point_3 PointType;
+
+  Point_3 origin = image->GetGeometry()->GetOrigin();
+  Point_3 endPoint = image->GetGeometry()->GetCornerPoint(7);
+
+  for (SeedPointDistanceToLabelsMap::const_iterator iter = lengths.begin();
+       iter != lengths.end(); ++iter)
+  {
+    const std::set<mitk::LabeledImageToTetrahedralGridFilter::LabelType>& labels = iter->second;
+
+    int xCount = (endPoint[0] - origin[0]) / iter->first;
+    std::vector<std::vector<std::pair<Point_3,mitk::LabeledImageToTetrahedralGridFilter::LabelType> > > seedPoints(omp_get_max_threads());
+    for (std::size_t i = 0; i < seedPoints.size(); ++i)
+    {
+      seedPoints[i].reserve(1000);
+    }
+
+    #pragma omp parallel for
+    for (int i = 0; i < xCount; ++i)
+    {
+      Point_3 seedPointCandidate = origin;
+      seedPointCandidate[0] = origin[0] + i * iter->first;
+      while (seedPointCandidate[1] < endPoint[1])
+      {
+        seedPointCandidate[2] = origin[2];
+        while (seedPointCandidate[2] < endPoint[2])
+        {
+          mitk::LabeledImageToTetrahedralGridFilter::LabelType label = const_cast<mitk::Image*>(image)->GetPixelValueByWorldCoordinate(seedPointCandidate);
+          if (label != 0 && labels.find(label) != labels.end())
+          {
+            seedPoints[omp_get_thread_num()].push_back(std::make_pair(seedPointCandidate, label));
+          }
+          seedPointCandidate[2] += iter->first;
+        }
+        seedPointCandidate[1] += iter->first;
+      }
+    }
+
+    for (std::size_t i = 0; i < seedPoints.size(); ++i)
+    {
+      for (std::size_t j = 0; j < seedPoints[i].size(); ++j)
+      {
+        const Point_3& seedPoint = seedPoints[i][j].first;
+        *pts++ = std::make_pair(PointType(seedPoint[0], seedPoint[1], seedPoint[2]), domain->index_from_subdomain_index(seedPoints[i][j].second));
+      }
+    }
+  }
+}
 
 int main(int argc, char *argv[])
 {
@@ -38,9 +97,9 @@ int main(int argc, char *argv[])
 	double special_size = 0.9; // Cell size to be used in subdomains of image with 'special_subdomain_label'
 	int special_subdomain_label = 0; // If this is set to zero, no subdomain resizing will be performed
 	int volume_dimension = 3;
-	
+
 	bool defulatcriteria = false;
-	
+
 	if (argc == 1) {
 		std::cout << " Enter the image stack file name (.inr): ";
 		std::cin >> inrfn;
@@ -80,7 +139,7 @@ int main(int argc, char *argv[])
 		outfn = argv[3];
 	else
 		outfn = "_tmp_image2mesh_cgal.mesh";
-		
+
 	image.read(inrfn.c_str());
 
 	// Domain
@@ -91,20 +150,58 @@ int main(int argc, char *argv[])
 	Sizing_field size(general_cell_size);
 	if (special_subdomain_label) {
 		std::cout << " Refining domain with label ID: " << special_subdomain_label << std::endl;
-		size.set_size(special_size, volume_dimension, 
+
+		size.set_size(special_size, volume_dimension,
 		              domain.index_from_subdomain_index(special_subdomain_label));
+		Sizing_field facetRadii(general_cell_size);
+		facetRadii.set_size(special_size, volume_dimension,
+		                    domain.index_from_subdomain_index(special_subdomain_label));
+		FacetCriteria facet_criteria(facet_angle,facetRadii,facet_distance);
+		CellCriteria cell_criteria(cell_radius_edge, size);
+		Mesh_criteria mesh_criteria(facet_criteria, cell_criteria);
+
+		typedef std::vector<std::pair<Point_3, Index> > initial_points_vector;
+		typedef initial_points_vector::iterator Ipv_iterator;
+		typedef C3t3::Vertex_handle Vertex_handle;
+
+		// Initialize c3t3
+  		C3t3 c3t3;
+
+		std::map<Index, double> subdomain_label_size;
+		subdomain_label_size.insert(std::make_pair(special_subdomain_label, special_size));
+
+		initial_points_vector initial_points;
+		ConstructSeedPoints(image, &domain, subdomain_label_size, std::back_inserter(initial_points));
+
+		if (initial_points.empty()) {
+			domain.construct_initial_points_object()(std::back_inserter(initial_points));
+			volume_dimension = 2;
+		}
+
+		for (Ipv_iterator it=initial_points.begin(); it!=initial_points.end(); ++it) {
+			Vertex_handle v = c3t3.triangulation().insert(it->first);
+			if (v != Vertex_handle()) {
+				c3t3.set_dimension(v, volume_dimension);
+				c3t3.set_index(v, it->second);
+			}
+		}
+		CGAL_assertion(c3t3.triangulation().dimension() == 3);
+		CGAL::refine_mesh_3(c3t3, domain, mesh_criteria, CGAL::parameters::no_reset_c3t3());
+		std::ofstream medit_file(outfn.c_str());
+		c3t3.output_to_medit(medit_file);
 	}
+	else {
+		// Mesh criteria
+		Mesh_criteria criteria(facet_angle, facet_size, facet_distance,
+		                       cell_radius_edge, cell_size=size);
 
-	// Mesh criteria
-	Mesh_criteria criteria(facet_angle, facet_size, facet_distance,
-	                       cell_radius_edge, cell_size=size);
+		// Meshing
+		C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria);
 
-	// Meshing
-	C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria);
-
-	// Output
-	std::ofstream medit_file(outfn.c_str());
-	c3t3.output_to_medit(medit_file);
+		// Output
+		std::ofstream medit_file(outfn.c_str());
+		c3t3.output_to_medit(medit_file);
+	}
 
 	return 0;
 }
